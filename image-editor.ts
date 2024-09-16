0473cee3f1f57e110641fc03c58654e094c488ee
James Finlinson <the30clues@gmail.com>
2024-09-16 11:39:32 -0600

    Begin project development

---
+++ image-editor.ts
@@ -0,0 +1,7 @@
+export default class ImageEditor {
+  constructor() {
+    console.log('ImageEditor constructor');
+  }
+
+
+}
---
+++ image.ts
@@ -0,0 +1,30 @@
+export class Color {
+  constructor(
+    public red = 0,
+    public green = 0,
+    public blue = 0,
+  ) { }
+}
+
+export class Image {
+  private pixels: Color[][];
+
+  constructor(width: number, height: number) {
+    this.pixels = [...Array(height)].map(() => [...Array(width)].map(() => new Color()));
+  }
+
+  getWidth() {
+    return this.pixels.length;
+  }
+  getHeight() {
+    return this.pixels[0].length;
+  }
+
+  set(x: number, y: number, color: Color) {
+    this.pixels[x][y] = color;
+  }
+
+  get(x: number, y: number) {
+    return this.pixels[x][y];
+  }
+}
