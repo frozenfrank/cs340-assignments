d2961c9d714c3282339e309db62924efdd0fc33c
James Finlinson <the30clues@gmail.com>
2024-10-09 11:14:17 -0600

    Extract static methods to generic Animal

--- src/entity/abstract/Animal.ts
+++ src/entity/abstract/Animal.ts
@@ -6,4 +6,17 @@ export abstract class Animal {
     this.trainingPriority = trainingPriority;
     this.name = name;
   }
+
+  static getAnimalsSorted<T extends Animal>(animalList: T[]) {
+    return animalList.sort((animal1, animal2) =>
+      animal1.trainingPriority < animal2.trainingPriority ? -1 : 1
+    );
+  }
+  static getAnimalsPriorityList<T extends Animal>(animalList: T[]): string {
+    return animalList
+      .map(
+        (animal) => animal.name + "'s training priority: " + animal.trainingPriority + "\n"
+      )
+      .join("");
+  }
 }
