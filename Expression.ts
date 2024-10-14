9b05ca4a2f914e6f2d4af463cde781db57b33fa2
James Finlinson <the30clues@gmail.com>
2024-10-14 11:09:07 -0600

    Refactor Expression

--- Expression.ts
+++ Expression.ts
@@ -2,14 +2,31 @@
 // 1. What design principles does this code violate?
 // 2. Refactor the code to improve its design.
 
-function isLowRiskClient(score: number, income: number, authorized: boolean): boolean {
-	if (!((score > 700) ||
-		((income >= 40000) && (income <= 100000) && authorized && (score > 500)) ||
-		(income > 100000))) {
+/**
+ * # Answers
+ * 1. Simplicity. Single responsibility responsibility. Decomposition. Readability.
+ * 2. Completed below.
+ */
 
-		return false;
-	}
-	else {
-		return true;
-	}
+function isLowRiskClient(score: number, income: number, authorized: boolean): boolean {
+	return isHighCreditScore(score) ||
+		isHighIncomeClient(income) ||
+		(authorized && isMediumIncomeClient(income) && isAtLeastMediumCreditScore(score));
+}
+
+
+function isAtLeastMediumCreditScore(score: number): boolean {
+	return score > 500;
+}
+
+function isHighCreditScore(score: number): boolean {
+	return score > 700;
+}
+
+function isMediumIncomeClient(income: number): boolean {
+	return (income >= 40000) && (income <= 100000);
+}
+
+function isHighIncomeClient(income: number): boolean {
+	return income > 100000;
 }
