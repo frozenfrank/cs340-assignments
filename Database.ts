59bcd8bdd275eedc7f4f62dccaaeef7b45c85b92
James Finlinson <the30clues@gmail.com>
2024-10-14 10:51:14 -0600

    Answer Database questions

--- Database.ts
+++ Database.ts
@@ -2,6 +2,12 @@
 // 1. What design principle(s) does this code violate?
 // 2. Explain how you would refactor this code to improve its design.
 
+/**
+ * # Answers
+ * 1. Single Responsibility Principle. It is combining the `Course` model with the course DAO methods.
+ * 2. I would remove the `create()`, `find()`, and `update()` methods and put them in a separate DAO class.
+ */
+
 export class Course {
 
 	name: string;
