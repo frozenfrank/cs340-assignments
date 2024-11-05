29d775bc74f19b922a3f602fe57ac3c5e871ac19
James Finlinson <the30clues@gmail.com>
2024-11-04 23:02:08 -0700

    Remove unnecessary state variables from Array2DFile

--- src/Array2DFile.ts
+++ src/Array2DFile.ts
@@ -2,27 +2,22 @@ import * as fs from "fs";
 import { Array2D } from "./Array2D";
 
 export class Array2DFile implements Array2D {
-  private rows: number;
-  private cols: number;
-  private filename: string;
   private _data: number[][];
 
   constructor()
   constructor(filename: string)
-  constructor(rows: number, cols: number)
-  constructor(rowsOrFilename?: string | number, cols?: number) {
+  constructor(rows: number, columns: number)
+  constructor(rowsOrFilename?: string | number, columns?: number) {
     if (typeof rowsOrFilename === "string") {
-      this.filename = rowsOrFilename;
-      this.load(this.filename);
+      const filename = rowsOrFilename;
+      this.load(filename);
     } else {
+      let rows = 0, cols = 0;
       if (typeof rowsOrFilename === "number") {
-        this.rows = rowsOrFilename;
-        this.cols = cols;
-      } else {
-        this.rows = 0;
-        this.cols = 0;
+        rows = rowsOrFilename;
+        cols = columns;
       }
-      this.initData(this.rows, this.cols);
+      this.initData(rows, cols);
     }
   }
 
