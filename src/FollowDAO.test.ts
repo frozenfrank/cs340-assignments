c085af657e24ee6fac286996c178fb3f49acaa31
James Finlinson <the30clues@gmail.com>
2024-11-07 03:41:51 -0700

    Clean up test organization

--- src/FollowDAO.test.ts
+++ src/FollowDAO.test.ts
@@ -61,26 +61,39 @@ async function updateFollow() {
     followee_name: "NEW: " + defaultFolloweeName,
   };
   await followDao.updateFollow(updateFollow);
+  console.log("updateDefaultFollow()");
 }
 
-async function deleteFollow() {
+async function deleteDefaultFollow() {
   await followDao.deleteFollow({
     followee_handle: defaultFolloweeHandle,
     follower_handle: defaultFollowerHandle
   });
+  console.log("deleteDefaultFollow()");
+}
+
+async function demonstrateWriteCapabilities() {
+  console.log("\nWriting items with same follower:");
+  await putItemsWithSameFollower();
+
+  console.log("\nWriting items with same followee:");
+  await putItemsWithSameFollowee();
+}
+
+async function demonstrateCrudOperations() {
+  console.log("\nDemonstrating CRUD operations:");
+  await getFollow(); // Status Update!
+  await createDefaultFollow();
+  await getFollow(); // Status Update!
+  await updateFollow();
+  await getFollow(); // Status Update!
+  await deleteDefaultFollow();
+  await getFollow(); // Status Update!
 }
 
 async function main() {
-  await putItemsWithSameFollower();
-
-  await putItemsWithSameFollowee();
-
-  await getFollow();
-
-  await createDefaultFollow();
-  await updateFollow();
-
-  await deleteFollow();
+  await demonstrateWriteCapabilities();
+  await demonstrateCrudOperations();
 }
 
 main();
