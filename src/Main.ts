abdd78bbac4bc286a8d33f7018c7c05c28d5008e
James Finlinson <the30clues@gmail.com>
2024-11-18 11:13:30 -0700

    Extend main to demonstrate all holidays

--- src/Main.ts
+++ src/Main.ts
@@ -1,11 +1,19 @@
 import { DecorationPlacer } from "./decoration/DecorationPlacer";
+import { BirthdayFactory } from "./holiday/Birthday/BirthdayProviderFactory";
+import { ChristmasFactory } from "./holiday/Christmas/ChristmasProviderFactory";
 import { HalloweenFactory } from "./holiday/Halloween/HalloweenProviderFactory";
+import { HolidayProviderFactory } from "./model/HolidayProviderFactory";
 
 main();
 
 function main(): void {
-  const holidayProvider = HalloweenFactory;
-  let decorationPlacer = new DecorationPlacer(holidayProvider);
+  celebrateHoliday(HalloweenFactory);
+  celebrateHoliday(BirthdayFactory);
+  celebrateHoliday(ChristmasFactory);
+}
 
+function celebrateHoliday(holidayProvider: HolidayProviderFactory) {
+  const decorationPlacer = new DecorationPlacer(holidayProvider);
   console.log(decorationPlacer.placeDecorations());
+  console.log("");
 }
