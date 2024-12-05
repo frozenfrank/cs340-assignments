8e8ea333299818e954a6287a23a37cb977bcbcf6
James Finlinson <the30clues@gmail.com>
2024-12-04 23:41:05 -0700

    Include FibonacciStringSource.ts

---
+++ src/string/FibonacciStringSource.ts
@@ -0,0 +1,16 @@
+import { StringSource } from "./StringSource";
+
+export class FibonacciStringSource implements StringSource {
+  private last = "";
+
+  private n1 = 0;
+  private n2 = 0;
+
+  next(): Promise<string> {
+    const n0 = this.n1 + this.n2;
+    this.last += n0;
+    [this.n1, this.n2] = [this.n2, n0];
+    return Promise.resolve(this.last);
+  }
+
+}
