69d090cdfb3b65483ac8163c9be4f2e13197d1bb
James Finlinson <the30clues@gmail.com>
2024-12-04 23:36:20 -0700

    Implement JokeStringSource

---
+++ src/string/JokeStringSource.ts
@@ -0,0 +1,8 @@
+import { StringSource } from "./StringSource";
+
+export class JokeStringSource implements StringSource {
+  async next(): Promise<string> {
+    const request = await fetch("https://v2.jokeapi.dev/joke/Misc,Programming?format=xml&safe-mode&type=single");
+    return request.text();
+  }
+}
